209db3b9ed7e37ea65c367cc9ed65925c50c9b02
Wijayasiri B.M.G <it21507960@my.sliit.lk>
2023-05-06 14:47:43 +0530

    del

--- trymad/app/src/main/java/com/example/mad/EmployeeModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mad
-//dbfeilds
-data class EmployeeModel(
-    var empId: String? = null,
-    var empName: String? = null,
-    var empAge: String? = null,
-    var empSalary: String? = null
-)
